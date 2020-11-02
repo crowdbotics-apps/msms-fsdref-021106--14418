dd60825317cd16e1c7e45a4536613d5b46590458
Crowdbotics <team@crowdbotics.com>
2020-11-02 09:27:58 +0000

    Update models

--- backend/msms_fsdref_021106__14418/wsgi.py
+++ backend/msms_fsdref_021106__14418/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msms_fsdref_021106__14418.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "msms_fsdref_021106__14418.settings")
 
 application = get_wsgi_application()
